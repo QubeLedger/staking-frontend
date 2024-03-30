168d5f153ff724b484bb2a016aa8d0976bb9eb9a
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-03-30 21:58:43 +0500

    fix: claim page button

--- src/components/Buttons/PageButtons/ClaimPageConfirm.tsx
+++ src/components/Buttons/PageButtons/ClaimPageConfirm.tsx
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const Button = styled.button`
     width: 100%;
-    height: 55px;
+    height: 50px;
+    font-size: 16px;
     background: linear-gradient(to right, #74BCFD, #339BFE);
     border: none;
-    font-size: 22px;
     font-weight: 600;
     color: white;
-    border-radius: 20px;
+    border-radius: 12px;
     margin-top: 20px;
     font-family: 'Inter', sans-serif;
     cursor: pointer;
